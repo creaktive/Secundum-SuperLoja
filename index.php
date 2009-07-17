a7e7729e2ba93012a78fd3c70035120c1304e8d5
Stanislaw Pusep <creaktive@gmail.com>
2009-07-17 08:44:54 -0300

    v2.1

--- index.php
+++ index.php
@@ -12,7 +12,16 @@ $chave = $_SERVER['HTTP_REFERER'];
 $chave = str_replace("+","_",$chave); $chave = str_replace(" ","_",$chave);
 if($chave) { $keyw = explode("q=",$chave); if($keyw[1]) { $chave = $keyw[1]; $keyw = explode("&",$chave); $chave = $keyw[0]; } else { $chave = ""; } }
 
-$endereco = "http://secundum.com.br/superloja/?p1=".$p1."&p2=".$busca."&p3=".$filtro."&p4=".$completa."&p5=".$chave;
+$parceiro = "http://secundum.com.br/superloja/parceiro.php?p1=".$p1;
+
+$ch = curl_init();$timeout = 15;
+curl_setopt ($ch, CURLOPT_URL, $parceiro);
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+$servidor = curl_exec($ch);
+curl_close($ch);
+
+$endereco = $servidor."?p1=".$p1."&p2=".$busca."&p3=".$filtro."&p4=".$completa."&p5=".$chave;
 
 $ch = curl_init();$timeout = 15;
 curl_setopt ($ch, CURLOPT_URL, $endereco);
