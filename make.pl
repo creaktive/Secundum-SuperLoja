18c7ac1b54fe0ed386b9288a95699a01712c221f
Stanislaw Pusep <creaktive@gmail.com>
2009-08-11 18:07:50 -0300

    GPLized

--- make.pl
+++ make.pl
@@ -44,6 +44,25 @@ binmode INST;
 ############ PHP CODE ############
 print INST<<HEADER
 <?php
+/*
+	Secundum SuperLoja - loja online utilizando o sistema Secundum
+	em parceria com o Mercado Livre para rentabilizar blogs/sites.
+	Copyright (C) 2009  Stanislaw Pusep
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 error_reporting(0);
 
 \$pkg = array(
